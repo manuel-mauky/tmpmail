d6fd24d75b969b9bb016d6dc4bd7422acdd9a733
Manuel Mauky <manuel.mauky@googlemail.com>
2012-11-09 17:21:03 +0100

    added interfaces for filtering and forwarding emails

---
+++ src/main/java/eu/lestard/tmpmail/core/ForwardingService.java
@@ -0,0 +1,24 @@
+package eu.lestard.tmpmail.core;
+
+import javax.mail.internet.MimeMessage;
+
+import eu.lestard.tmpmail.persistence.TempEmailAddress;
+
+/**
+ * This component is used to forward emails to there real receiver.
+ * 
+ * @author manuel.mauky
+ * 
+ */
+public interface ForwardingService {
+
+	/**
+	 * Forward the given message to the email address of the user that has registered the given
+	 * temporary email address mapping.
+	 * 
+	 * @param originalMessage
+	 * @param tempEmailAddress
+	 */
+	void forwardMessage(MimeMessage originalMessage, TempEmailAddress tempEmailAddress);
+
+}
---
+++ src/main/java/eu/lestard/tmpmail/core/MailFilterService.java
@@ -0,0 +1,29 @@
+package eu.lestard.tmpmail.core;
+
+import javax.mail.internet.MimeMessage;
+
+/**
+ * This component is used to handle incomming email messages.
+ * 
+ * @author manuel.mauky
+ * 
+ */
+public interface MailFilterService {
+
+	/**
+	 * This method is used to handle an incomming email message and give it to the forwarding
+	 * mechanism when there is a mapping persisted that matches the given message.
+	 * 
+	 * At first it is checked whether the domain part of the receiver is handled by this
+	 * application.
+	 * 
+	 * After that it is checked whether there is a mapping persisted for this domain part.
+	 * 
+	 * When the receivers domain is not known or there is no mapping persisted, the email is
+	 * discarded without response.
+	 * 
+	 * @param emailMessage
+	 */
+	void filterEmail(MimeMessage emailMessage);
+
+}
