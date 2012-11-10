472108288d2745f04c18499f91294f63604fc828
Manuel Mauky <manuel.mauky@googlemail.com>
2012-11-10 22:05:38 +0100

    added default value setting in Configurator

--- src/main/java/eu/lestard/tmpmail/config/internal/ConfiguratorImpl.java
+++ src/main/java/eu/lestard/tmpmail/config/internal/ConfiguratorImpl.java
@@ -2,6 +2,7 @@ package eu.lestard.tmpmail.config.internal;
 
 import java.util.EnumMap;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 
 import eu.lestard.tmpmail.config.IntKey;
@@ -21,6 +22,17 @@ public class ConfiguratorImpl implements Configurator {
 	private final EnumMap<IntKey, Integer> integerValues = new EnumMap<IntKey, Integer>(IntKey.class);
 	private final EnumMap<StringKey, String> stringValues = new EnumMap<StringKey, String>(StringKey.class);
 
+
+	/**
+	 * This method needs to be replaces by a component that reads a properties
+	 * file or something.
+	 */
+	@PostConstruct
+	public void loadDefaultValues() {
+		setValue(IntKey.INCOMMING_SMTP_PORT, 25000);
+		setValue(IntKey.OUTGOING_SMTP_PORT, 20000);
+		setValue(StringKey.OUTGOING_SMTP_HOST, "localhost");
+	}
 
 	@Override
 	public Integer getValue(final IntKey key) {
